484d70e08e362a1f3891cb44b585dd89038d98e8
Netznarkose <mmaintz@posteo.de>
2015-01-27 11:01:46 +0100

    add gustavo warmup-003

---
+++ warmup-003/warmup-003.rb
@@ -0,0 +1,24 @@
+# Go to your ruby colearning warmup exercises folder
+# Once there create a folder name warmup-003
+# Inside this folder create two files: one titled warmup-003.rb and the other
+# warmup-003_spec.rb
+
+# In warmup-003.rb, your task is to create a method called first_even_number
+# that takes an array of numbers as an argument and returns the first even number
+# of this array.
+
+# Bonus point: in warmup-003_spec.rb create tests for the first_even_number method you created
+
+# Push your code to Github
+
+def first_even_number dummy_array
+  dummy_array.each do |i|
+    test = i.even?
+      if test == true
+        return i 
+#        break
+      end   
+     end
+  nil
+  end
+
---
+++ warmup-003/warmup-003_spec.rb
@@ -0,0 +1,19 @@
+require_relative "./warmup-003"
+
+describe "#first_even_number" do  # method for debugging
+  it "should return the first even number in an array" do
+    expect(first_even_number([30, 17, 77, 102])).to eq(30)
+  end
+
+  it "should return the first even number in an array" do
+    expect(first_even_number([5, 11, 50])).to eq(50)
+  end
+
+  it "should return the first even number in an array" do
+    expect(first_even_number([3, 5, 15, 16, 23, 42])).to eq(16)
+  end
+  it "should return nil" do
+    expect(first_even_number([3, 5 ,10, 15, 23, ])).to eq(nil)
+  end
+
+end
