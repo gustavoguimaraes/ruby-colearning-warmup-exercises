a02454cca5926f0a97e59987eae526d19f316e01
Netznarkose <mmaintz@posteo.de>
2015-01-28 16:00:13 +0100

    + 1

---
+++ oop_examples/rails_tutorial_oop_example4.2.rb
@@ -0,0 +1,11 @@
+class Fixnum
+  def klavier
+  var = self %2 == true
+    if var == true
+      puts "ja, ist even sagt meister max"
+    else
+      puts "nein, ist nicht even, sagt keule"
+    end
+  end
+end
+
