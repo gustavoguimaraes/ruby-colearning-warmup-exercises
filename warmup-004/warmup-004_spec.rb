57497923c4f58d215945f383ac4f841c6e8b2c61
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2015-02-02 18:11:56 +0100

    Warm-004

---
+++ warmup-004/warmup-004_spec.rb
@@ -0,0 +1,10 @@
+require_relative "./warmup-004"
+
+describe "#count_words" do
+  it "should return the number of words in a sentence" do
+    expect(count_words("I am a sentence with seven words")).to eq(7)
+  end
+
+  #write at least one other test here
+
+end
