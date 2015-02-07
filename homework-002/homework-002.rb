1e04179a8291937bec79cc91f0e290a7fb0978c1
Netznarkose <mmaintz@posteo.de>
2015-02-07 17:26:44 +0100

    homework-002 commit1

--- homework-002/homework-002.rb
+++ homework-002/homework-002.rb
@@ -10,5 +10,37 @@ class Kitchen
   end
 
   #write your methods that make the tests pass here
+ def add shovel
+    @items = @items.push shovel 
+  end 
 
+  def add_bang
+    @items = @items.map do |i| 
+      i.concat "!" 
+    end 
+  end  
+
+  def remove_if_more_than_three_letters data
+   data.each do |i|
+      if i.length < 4
+        @items = @items.push i
+      end
+    end 
+    @items 
+  end
+
+  def alphabetically_backwards data
+    @items = data.sort.reverse
+  end
+
+  def first_three data
+    @items = data[0..2]
+  end
+
+  def remove_first_and_last_elements
+    var = @items
+    var.pop
+    var.shift
+    @items = var
+  end
 end
