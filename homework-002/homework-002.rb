7514d2523dc2b5007a9b129305bd7f90dc1ce1d5
Netznarkose <mmaintz@posteo.de>
2015-02-07 16:12:18 +0100

    initialized homework-002

---
+++ homework-002/homework-002.rb
@@ -0,0 +1,14 @@
+class Kitchen
+
+  attr_accessor :items
+  def initialize
+    @items = []
+  end
+
+  def items
+    @items
+  end
+
+  #write your methods that make the tests pass here
+
+end
---
+++ homework-002x/README.md
@@ -0,0 +1,4 @@
+Run homework-002_spec.rb
+$ rspec homework-002_spec.rb
+
+You will see a bunch of tests failing. Make them pass.
