129a3f8f3d6e1555f87140da564d5c1a2dc4e02d
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2015-02-12 17:54:08 +0100

    warmup-006 added

---
+++ warmup-006/warmup-006.rb
@@ -0,0 +1,11 @@
+# Create a class called Spaetkauf that has one instance variable: line.
+
+# In our Spaeti, we should be able to call "take a number" that takes a customer's name,
+# appends their number to their name and adds them to the line.
+
+# The line could look like this: ["1. Gustavo", "2. Max", "3. Duilio"]
+
+# Additionally we should be able to call a method called "now_serving" that
+# removes the customer who is first in line and returns their name.
+
+# Write a test and then write the class.
---
+++ warmup-006/warmup-006_spec.rb
@@ -0,0 +1,13 @@
+require './warmup-006.rb'
+
+describe Spaetkauf  do
+
+  describe "#take_number" do
+    #write your tests for this method here
+  end
+
+  describe '#serving_now' do
+    #write your tests for this method here
+  end
+
+end
