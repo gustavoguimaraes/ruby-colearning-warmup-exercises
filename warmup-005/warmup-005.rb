e5a4e2c64794dfa25bcfd1f06adb9813d543fbbc
Netznarkose <mmaintz@posteo.de>
2015-02-12 11:41:20 +0100

    refactoring warmup-005 3

--- warmup-005/warmup-005.rb
+++ warmup-005/warmup-005.rb
@@ -11,7 +11,7 @@ def roll_bucket(dice_sides)
   dice_sides.map {|i| roll_die(i)}
 end
 
-def on_the_table(rolled_dice)
+def on_the_table(rolled_dice) # expects an hash of arrays
   expected_result = ""
   rolled_dice.each do |i| 
     expected_result << "On the table:\n Your #{i[:sides]}-sided die shows the value of #{i[:face]}\n"
@@ -21,6 +21,23 @@ def on_the_table(rolled_dice)
   end
   expected_result
 end
+
+result = []
+data = []
+begin 
+  puts """reate as many dice you want
+  A die should have minimum 4 sides
+  Enter 0 to roll the dice
+  How many sides?"""
+  var = gets.chomp.to_i
+  if var == 0
+    data = roll_bucket result
+    puts on_the_table data
+  else
+    result << var
+    puts "Added a die with #{var} sides"
+  end
+end until var == 23
 
 # In a loop, ask the user for the dice sides
 
