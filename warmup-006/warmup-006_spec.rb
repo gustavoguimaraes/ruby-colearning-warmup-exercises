5932354f3684bb8f82b3149c6377816c1c2ef2ad
Netznarkose <mmaintz@posteo.de>
2015-02-18 18:20:44 +0100

    warmup-006

---
+++ warmup-006/warmup-006_spec.rb
@@ -0,0 +1,15 @@
+require './warmup-006.rb'
+
+describe Spaetkauf do
+  let(:spaetkauf) { Spaetkauf.new }
+
+    it "adds an indexed element" do
+      expect(spaetkauf.take_number("piano")).to eq ["1. piano"]
+    end 
+    it "shows first element and deletes it" do
+      spaetkauf.take_number("piano")
+      spaetkauf.take_number("timpani")
+      spaetkauf.take_number("trumpet")
+      expect(spaetkauf.now_serving).to eq ["1. piano"]
+    end 
+end
