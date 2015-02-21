e14b0c239426f28bbc1546e0beef78d3b8bc24d7
Netznarkose <mmaintz@posteo.de>
2015-02-21 14:54:47 +0100

    blackjack.rb 6

--- blackjack_dashboard.rb
+++ blackjack_dashboard.rb
@@ -1,5 +1,17 @@
 require_relative 'blackjack.rb'
 
+def player_shows_cards
+  @player.hand.map { |var| var.card_value }
+end
+# dealer show cards (one card up one down)
+def dealer_shows_cards
+  @dealer.hand.map { |var| var.card_value if var.card_face == "up" }
+end
+def dealer_reveal_cards
+  @dealer.hand.map { |var| var.card_value }
+end
+
+
 # place bet
 
 # player karten austeilen
@@ -15,15 +27,7 @@ end
 # exception, wenn zwei 11 dann ist eine eine eins
 
 
-# player show cards 
-def player_shows_cards
-  @player.hand.map { |var| var.card_value }
-end
-# dealer show cards (one card up one down)
-def dealer_shows_cards
-  @dealer.hand.map { |var| var.card_value if var.card_face == "up" }
-end
-
+begin
 print player_shows_cards
 print dealer_shows_cards
 
@@ -33,42 +37,33 @@ if @dealer.black == :blackjack
 else
   # insurance when face up card = 11
 end 
-
 # player acts
 puts "you wanne 1. hit or 2. stand"
 choice = gets.chomp.to_i
 if choice == 1
   @player.add_cards "up" # hit
-  print player_shows_cards
   if @player.black == :busted
     puts "you busted, bastard"
   end
 elsif choice == 2
   #stand
+  begin
+    condition = ""
+    if @dealer.count <= 21 && @dealer.count > @player.count 
+      condition = "Dealer wins!"
+    elsif @dealer.count < 21 && @dealer.count < @player.count
+      @dealer.add_cards "down"
+    elsif @dealer.count > 21  
+      condition = "Player wins!"
+    elsif @dealer.count == @player.count
+      condition = "It's a Tie!"
+    end 
+  end until condition == "Dealer wins!" || condition == "Player wins!" || condition == "It's a Tie!"
 else
   puts "invalid entry"
 end
-
-#dealer acts
-begin
-  if @dealer.black == :unbusted && @dealer.count > @player.count
-    puts "Dealer wins!"
-    break
-  elsif @dealer.black == :unbusted && @dealer.count < @player.count
-    @dealer.add_cards "down"
-  else
-    puts "Player wins!"
-  end
-end
-
-
-
-
-
-
-
-
-
-
+end while choice == 1
+print dealer_reveal_cards
+puts condition
 
 
